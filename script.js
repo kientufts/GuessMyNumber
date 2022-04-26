44a1e42899afbba10039403225ea4e03c2607abb
Kien Le <letrungkien1991@gmail.com>
2022-04-26 00:24:42 -0700

    Basic game logic and cleaning up the code

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
 'use strict';
-/*
-console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.message').textContent = 'Correct Number 😍';
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 19;
-
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
-*/
+let gameScore = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  // When there is no number entered
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No number!';
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number 😍';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    // When guess is too high
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '📈 Too High!';
+    gameScore--;
+
+    // When guess is too low
+  } else {
+    document.querySelector('.message').textContent = '📉 Too Low!';
+    gameScore--;
   }
+
+  // Avoid score to be negative
+  if (gameScore <= 0) {
+    document.querySelector('.message').textContent = '💥 You lost the game!';
+    gameScore = 0;
+  }
+  document.querySelector('.score').textContent = gameScore;
+
+  // FIXME: Stop the game when score = 0
 });
