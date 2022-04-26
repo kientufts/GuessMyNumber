f34977a397bd021f71e44b21b01a1a756d898d53
Kien Le <letrungkien1991@gmail.com>
2022-04-26 00:29:37 -0700

    fix to hide the secret number

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
 
 let gameScore = 20;
 
@@ -20,6 +19,8 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
 
     document.querySelector('.number').style.width = '30rem';
+
+    document.querySelector('.number').textContent = secretNumber;
 
     // When guess is too high
   } else if (guess > secretNumber) {
